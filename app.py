616a80cc10624186cc93d93e93e8247d9f339341
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-04-20 11:39:55 +0200

    He terminado inicio.html, he sumado al programa python principal la funciones de potencia y he creado los dos nuevos html que van a proporcionar la potencia.

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
 def inicio():
     nombre = "Carlos Manuel"
     return render_template("inicio.html",nombre=nombre)
+
+@app.route('/potencia',methods=["GET","POST"])
+def potencia():
+    return render_template("potencia.html")
+
+@app.route("/potencia/<base>/<expo>",methods=["GET","POST"])
+def operar_potencia (base,expo):
+    base = int(base)
+    expo = int(expo)
+    if expo == 0:
+        resultado = 1
+    elif expo < 0:
+        resultado = 1/(base**-(expo))
+    elif expo > 0:
+        resultado = base ** expo
+    else:
+        abort(404)
+    return render_template("operar_potencia.html",num1=base,num2=expo,num3=resultado)
 
 app.run("0.0.0.0",5000,debug=True)

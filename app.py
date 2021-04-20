ebb924f392b09f5994a90637ecb4cb29d818cb29
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-04-20 15:29:16 +0200

    He añadido la función cuenta letras con sus respectivos ficheros .html, también he modificado los parrafos de fichero inicio y descargado el fichero .xml que se utulizara en la última función.

--- app.py
+++ app.py
@@ -24,4 +24,21 @@ def operar_potencia (base,expo):
         abort(404)
     return render_template("operar_potencia.html",num1=base,num2=expo,num3=resultado)
 
+@app.route('/cuenta_letras',methods=["GET","POST"])
+def cuenta ():
+    return render_template("letras.html")
+
+@app.route("/cuenta_letras/<palabra>/<letra>",methods=["GET","POST"])
+def cuenta_letra (palabra,letra):
+    if len(letra) > 1:
+        abort(404)
+    else:
+        cont = palabra.count(letra)
+    
+    if cont == 1:
+        palabra2 = "vez"
+    else:
+        palabra2 = "veces"
+    return render_template("cuenta_letra.html",pa1=palabra,pa2=letra,pa3=palabra2,resu=cont)
+
 app.run("0.0.0.0",5000,debug=True)
